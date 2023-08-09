5a5cef5e5c628d78e6d04c40fc9398cba0eb5779
ZEROITRAYO <dhaivatrpatel@gmail.com>
2023-08-09 17:21:46 +0530

    assignments added

--- assignments/08.08.2023/78.cpp
+++ assignments/08.08.2023/78.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    void recc(vector<int>& nums,vector<vector<int>>& ans,vector<int> temp,int i){
+    void solve(vector<int>& nums,vector<vector<int>>& ans,vector<int> temp,int i){
         if(i>=nums.size()){
             ans.push_back(temp);
             return;
         }
         else{
             temp.push_back(nums[i]);
-            recc(nums,ans,temp,i+1);
+            solve(nums,ans,temp,i+1);
             temp.pop_back();
-            recc(nums,ans,temp,i+1);
+            solve(nums,ans,temp,i+1);
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -17,7 +17,7 @@ public:
         vector<vector<int>> ans;
         vector<int> temp;
         
-        recc(nums,ans,temp,0);
+        solve(nums,ans,temp,0);
         return ans;
     }
 };
