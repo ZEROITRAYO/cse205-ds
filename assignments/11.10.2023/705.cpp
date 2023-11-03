ff6f7548930f2ab42ea2760c3043efc60b4f323d
ZEROITRAYO <dhaivatrpatel@gmail.com>
2023-11-03 09:05:47 +0530

    assignments added

--- assignments/11.10.2023/705.cpp
+++ assignments/11.10.2023/705.cpp
@@ -19,3 +19,10 @@ vector<bool> v;
     }
 };
 
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
